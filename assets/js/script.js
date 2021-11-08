cb7dd1f44b63d4cd6fbfe59cd5d527a833e92b70
Shinji <francisco.j.orrego@gmail.com>
2021-11-08 15:04:29 -0300

    se agrega el Proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+          $("a").ready(function(){
+               
+               $("a").on('click', function(event) {
+              
+               if (this.hash !== "") {
+               
+               event.preventDefault();
+               
+               var hash = this.hash;
+              
+               $('html, body').animate({
+               scrollTop: $(hash).offset().top
+               }, 800, function(){
+                    window.location.hash = hash;
+});
+} 
+});
+});
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
